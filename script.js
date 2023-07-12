9270c0ce71ceeab9e8ab533e77e220e926249c50
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 02:41:05 +0530

    innertext and made some changes

---
+++ script.js
@@ -0,0 +1,5 @@
+document.title = "New title"
+document.getElementById("main-header").style.borderBottom = "2px solid black";
+let title = document.querySelector("#main .title")
+title.style.fontWeight = "bold"
+title.style.color ="green"
