82501c8644179cb1c60a5f7972afac3236517a7c
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 02:46:19 +0530

    font element made bold

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById("main-header").style.borderBottom = "2px solid black";
 let title = document.querySelector("#main .title")
 title.style.fontWeight = "bold"
 title.style.color ="green"
+
+
+let li = document.getElementsByClassName("list-group-item");
+li[2].style.backgroundColor ="green"
+
+for(let i=0;i<li.length;i++){
+    li[i].style.fontWeight ="bold"
+}
